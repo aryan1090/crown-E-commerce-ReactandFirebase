c17edb4b61dc97b9a10ae63cf70b8a696b7e6edb
aryan1090 <aryanchowdhary2172@gmail.com>
2021-02-18 21:34:46 +0530

    made different components and styled menu-items component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default Homepage;
